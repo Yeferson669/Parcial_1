288aaeb09480e9827d5e2e83172a7ec40aa8f807
Yeferson669 <Ydguaca49@ucatolica.edu.co>
2025-10-28 01:46:08 -0500

    codigo main

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ from .db import Base, engine
 from .routers import empleados, proyectos
 
 app = FastAPI(title="Sistema de Gestión de Proyectos", version="1.0")
+
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(empleados.router)
+app.include_router(proyectos.router)
+
+@app.get("/", summary="Raíz")
+def root():
+    return {"message": "API Sistema de Gestión de Proyectos - /docs para documentación"}
